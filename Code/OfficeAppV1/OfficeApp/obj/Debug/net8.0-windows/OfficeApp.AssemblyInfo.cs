1dd7d0810044f38090ab4ba29ab5913bb231da3a
Asteron2007 <52139280+Asteron2007@users.noreply.github.com>
2026-01-23 20:30:43 +0300

    1

--- Code/OfficeAppV1/OfficeApp/obj/Debug/net8.0-windows/OfficeApp.AssemblyInfo.cs
+++ Code/OfficeAppV1/OfficeApp/obj/Debug/net8.0-windows/OfficeApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OfficeApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49c1c1dd7b464936a9f1b4d71f08b7864dc56acc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7ead3deea229e876924980858a2ed8ed73fada5")]
 [assembly: System.Reflection.AssemblyProductAttribute("OfficeApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OfficeApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
